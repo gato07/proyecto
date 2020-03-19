c50486bd160e0cce512d019d1d3f08df5fe8345a
Arturo Aguilar <kdt_gato07@hotmail.com>
2020-03-18 20:37:53 -0700

    clase Empleado

--- pebcs/CapaPresentación/MainWindow.xaml.cs
+++ pebcs/CapaPresentación/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CapaLogica;
 
 namespace CapaPresentación
 {
@@ -32,8 +33,7 @@ namespace CapaPresentación
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            Menu_Principal Ventana = new Menu_Principal();
-            Ventana.ShowDialog();
+          
         }
     }
 }
