0187449c48a0a034c3aebc778e964633a8a58603
Arturo Aguilar <kdt_gato07@hotmail.com>
2020-08-22 17:16:24 -0600

    Agregar Clase de datos y logica de Dictamen_Estimacion

---
+++ pebcs/CapaAccesoDatos/dtsDictamen_Estimacion.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace CapaAccesoDatos
+{
+    public class dtsDictamen_Estimacion
+    {
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Propiedades
+
+        public int Numero { get; set; }
+        public bool Tipo { get; set; }
+        public DateTime Fecha_Registro { get; set; }
+        public DateTime Visita_Programada { get; set; }
+        public bool Elaboracion { get; set; }
+        public string Observacion_Elaboracion { get; set; }
+        public bool Entregado { get; set; }
+        public bool Manifestacion { get; set; }
+        public bool Oficio_Subdivision { get; set; }
+        public bool Escrituras { get; set; }
+        public bool Licencia_Construccion { get; set; }
+        public bool Otra { get; set; }
+        public string Otra_Nombre { get; set; }
+        public int Id_Cliente { get; set; }
+        public int Clave_Inmueble { get; set; }
+        public int Clave_Empleado { get; set; }
+        public bool Eliminado { get; set; }
+        public bool Existe { get; set; }
+
+        #endregion Propiedades
+
+        #region Metodos
+
+        public dtsDictamen_Estimacion()
+        {
+            try
+            {
+
+                Numero = 0;
+                Tipo = false;
+                Fecha_Registro = new DateTime();
+                Visita_Programada = new DateTime();
+                Elaboracion = false;
+                Observacion_Elaboracion = "";
+                Entregado = false;
+                Manifestacion = false;
+                Oficio_Subdivision = false;
+                Escrituras = false;
+                Licencia_Construccion = false;
+                Otra = false;
+                Otra_Nombre = "";
+                Id_Cliente = 0;
+                Clave_Inmueble = 0;
+                Clave_Empleado = 0;
+                Eliminado = false;
+                Existe = false;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public dtsDictamen_Estimacion(int Numero)
+        {
+            try
+            {
+                this.Numero = 0;
+                Tipo = false;
+                Fecha_Registro = new DateTime();
+                Visita_Programada = new DateTime();
+                Elaboracion = false;
+                Observacion_Elaboracion = "";
+                Entregado = false;
+                Manifestacion = false;
+                Oficio_Subdivision = false;
+                Escrituras = false;
+                Licencia_Construccion = false;
+                Otra = false;
+                Otra_Nombre = "";
+                Id_Cliente = 0;
+                Clave_Inmueble = 0;
+                Clave_Empleado = 0;
+                Eliminado = false;
+                Existe = false;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                DataTable dt = conexion.Consulta_Seleccion("CALL SP_DictEsti_SelXNumero(" + Numero + ");").Tables[0];
+                if (dt != null)
+                {
+                    this.Numero = Convert.ToInt16(dt.Rows[0]["Numero"]);
+                    Tipo = Convert.ToBoolean(dt.Rows[0]["Tipo"]);
+                    Fecha_Registro = Convert.ToDateTime(dt.Rows[0]["Fecha_Registro"]);
+                    Visita_Programada = Convert.ToDateTime(dt.Rows[0]["Visita_Programada"]);
+                    Elaboracion = Convert.ToBoolean(dt.Rows[0]["Elaboracion"]);
+                    Observacion_Elaboracion = dt.Rows[0]["Observacion_Elaboracion"].ToString();
+                    Entregado = Convert.ToBoolean(dt.Rows[0]["Entregado"]);
+                    Manifestacion = Convert.ToBoolean(dt.Rows[0]["Manifestacion"]);
+                    Oficio_Subdivision = Convert.ToBoolean(dt.Rows[0]["Oficio_Subdivision"]);
+                    Escrituras = Convert.ToBoolean(dt.Rows[0]["Escrituras"]);
+                    Licencia_Construccion = Convert.ToBoolean(dt.Rows[0]["Licencia_Construccion"]);
+                    Otra = Convert.ToBoolean(dt.Rows[0]["Otra"]);
+                    Otra_Nombre = dt.Rows[0]["Otra_Nombre"].ToString();
+                    Id_Cliente = Convert.ToInt16(dt.Rows[0]["Id_Cliente"]);
+                    Clave_Inmueble = Convert.ToInt16(dt.Rows[0]["Clave_Inmueble"]);
+                    Clave_Empleado = Convert.ToInt16(dt.Rows[0]["Clave_Empleado"]);
+                    Eliminado = Convert.ToBoolean(dt.Rows[0]["Eliminado"]);
+                    Existe = true;
+                }
+                conexion.Desconectar();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public dtsDictamen_Estimacion(int Numero, bool Tipo, DateTime Fecha_Registro, DateTime Visita_Programada, 
+            bool Elaboracion, string Observacion_Elaboracion, bool Entregado, bool Manifestacion, 
+            bool Oficio_Subdivision, bool Escrituras, bool Licencia_Construccion, bool Otra, 
+            string Otra_Nombre, int Id_Cliente, int Clave_Inmueble, int Clave_Empleado)
+        {
+            try
+            {
+                this.Numero = Numero;
+                this.Tipo = Tipo;
+                this.Fecha_Registro = Fecha_Registro;
+                this.Visita_Programada = Visita_Programada;
+                this.Elaboracion = Elaboracion;
+                this.Observacion_Elaboracion = Observacion_Elaboracion;
+                this.Entregado = Entregado;
+                this.Manifestacion = Manifestacion;
+                this.Oficio_Subdivision = Oficio_Subdivision;
+                this.Escrituras = Escrituras;
+                this.Licencia_Construccion = Licencia_Construccion;
+                this.Otra = Otra;
+                this.Otra_Nombre = Otra_Nombre;
+                this.Id_Cliente = Id_Cliente;
+                this.Clave_Inmueble = Clave_Inmueble;
+                this.Clave_Empleado = Clave_Empleado;
+                Eliminado = false;
+                Existe = false;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public int dtsInsertar(bool Tipo, DateTime Fecha_Registro, DateTime Visita_Programada,
+            bool Elaboracion, string Observacion_Elaboracion, bool Entregado, bool Manifestacion,
+            bool Oficio_Subdivision, bool Escrituras, bool Licencia_Construccion, bool Otra,
+            string Otra_Nombre, int Id_Cliente, int Clave_Inmueble, int Clave_Empleado)
+        {
+            try
+            {
+                int res = 0;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                DataTable dt = conexion.Consulta_Seleccion("CALL SP_DictEsti_Insertar(" + Tipo + ",'"
+                    + Fecha_Registro.ToString("yyyy-MM-dd") + "','" + Visita_Programada.ToString("yyyy-MM-dd") 
+                    + "'," + Elaboracion + ",'" + Observacion_Elaboracion + "'," + Entregado + "," 
+                    + Manifestacion + "," + Oficio_Subdivision + "," + Escrituras + "," + Licencia_Construccion 
+                    + "," + Otra + ",'" + Otra_Nombre + "'," + Id_Cliente + "," + Clave_Inmueble + ","
+                    + Clave_Empleado + ");").Tables[0];
+                if (dt != null)
+                    res = Convert.ToInt16(dt.Rows[0]["Ultimo_Id"]);
+                conexion.Desconectar();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        public bool dtsActualizar(int Numero, bool Tipo, DateTime Fecha_Registro, DateTime Visita_Programada,
+            bool Elaboracion, string Observacion_Elaboracion, bool Entregado, bool Manifestacion,
+            bool Oficio_Subdivision, bool Escrituras, bool Licencia_Construccion, bool Otra,
+            string Otra_Nombre, int Id_Cliente, int Clave_Inmueble, int Clave_Empleado)
+        {
+            try
+            {
+                bool res = false;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                res = conexion.Consulta_Accion("CALL SP_DictEsti_Actualizar(" + Numero + "," + Tipo + ",'"
+                    + Fecha_Registro.ToString("yyyy-MM-dd") + "','" + Visita_Programada.ToString("yyyy-MM-dd")
+                    + "'," + Elaboracion + ",'" + Observacion_Elaboracion + "'," + Entregado + ","
+                    + Manifestacion + "," + Oficio_Subdivision + "," + Escrituras + "," + Licencia_Construccion
+                    + "," + Otra + ",'" + Otra_Nombre + "'," + Id_Cliente + "," + Clave_Inmueble + ","
+                    + Clave_Empleado + ");");
+                conexion.Desconectar();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool dtsEliminar(int Numero)
+        {
+            try
+            {
+                bool res = false;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                res = conexion.Consulta_Accion("CALL SP_DictEsti_Eliminar(" + Numero + ");");
+                conexion.Desconectar();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool dtsActivar(int Numero)
+        {
+            try
+            {
+                bool res = false;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                res = conexion.Consulta_Accion("CALL SP_DictEsti_Activar(" + Numero + ");");
+                conexion.Desconectar();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+
+        public DataTable dtsSelTodos()
+        {
+            try
+            {
+                DataTable dt = null;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                dt = conexion.Consulta_Seleccion("CALL SP_DictEsti_SelTodos();").Tables[0];
+                conexion.Desconectar();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        #endregion Metodos
+
+    }
+}
---
+++ pebcs/CapaLogica/Dictamen_Estimacion.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Data;
+using CapaAccesoDatos;
+using System.Text;
+
+namespace CapaLogica
+{
+    public class Dictamen_Estimacion : dtsDictamen_Estimacion
+    {
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Propiedades
+
+        public string Mensaje { get; set; }
+
+        #endregion Propiedades
+
+        #region Metodos
+
+        public Dictamen_Estimacion() : base()
+        {
+            try
+            {
+                Mensaje = "";
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el constructor del Dictamen/Estimación";
+            }
+        }
+
+        public Dictamen_Estimacion(int Numero) : base(Numero)
+        {
+            try
+            {
+                Mensaje = "";
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el constructor del Dictamen/Estimación";
+            }
+        }
+
+        public Dictamen_Estimacion(int Numero, bool Tipo, DateTime Fecha_Registro, DateTime Visita_Programada,
+            bool Elaboracion, string Observacion_Elaboracion, bool Entregado, bool Manifestacion,
+            bool Oficio_Subdivision, bool Escrituras, bool Licencia_Construccion, bool Otra,
+            string Otra_Nombre, int Id_Cliente, int Clave_Inmueble, int Clave_Empleado) :
+            base(Numero, Tipo, Fecha_Registro, Visita_Programada, Elaboracion, Observacion_Elaboracion, Entregado,
+                Manifestacion, Oficio_Subdivision, Escrituras, Licencia_Construccion, Otra, Otra_Nombre, Id_Cliente, 
+                Clave_Inmueble, Clave_Empleado)
+        {
+            try
+            {
+                Mensaje = "";
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el constructor del Dictamen/Estimación";
+            }
+        }
+
+        public int Insertar(bool Tipo, DateTime Fecha_Registro, DateTime Visita_Programada,
+            bool Elaboracion, string Observacion_Elaboracion, bool Entregado, bool Manifestacion,
+            bool Oficio_Subdivision, bool Escrituras, bool Licencia_Construccion, bool Otra,
+            string Otra_Nombre, int Id_Cliente, int Clave_Inmueble, int Clave_Empleado)
+        {
+            try
+            {
+                int res = 0;
+                Validacion validacion = new Validacion();
+                Mensaje = "Ocurrio un error en el proceso de dar de alta al Dictamen/Estimación, es posible que no se haya insertado"
+                    + " correctamente";
+                res = dtsInsertar(Tipo, Fecha_Registro, Visita_Programada, Elaboracion, Observacion_Elaboracion, Entregado,
+                Manifestacion, Oficio_Subdivision, Escrituras, Licencia_Construccion, Otra, Otra_Nombre, Id_Cliente,
+                Clave_Inmueble, Clave_Empleado);
+                if (res > 0)
+                    Mensaje = "El Dictamen/Estimación fue registrado satisfactoriamente";
+                return res;
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el proceso de dar de alta al Dictamen/Estimación, es posible que no se haya insertado"
+                    + " correctamente";
+                return 0;
+            }
+        }
+
+        public bool Actualizar(int Numero, bool Tipo, DateTime Fecha_Registro, DateTime Visita_Programada,
+            bool Elaboracion, string Observacion_Elaboracion, bool Entregado, bool Manifestacion,
+            bool Oficio_Subdivision, bool Escrituras, bool Licencia_Construccion, bool Otra,
+            string Otra_Nombre, int Id_Cliente, int Clave_Inmueble, int Clave_Empleado)
+        {
+            try
+            {
+                bool res = false;
+                Validacion validacion = new Validacion();
+                Mensaje = "Ocurrio un error en el proceso de actualización de datos del Dictamen/Estimación, es posible"
+                   + " que no se hayan modificado los datos correctamente";
+                res = dtsActualizar(Numero, Tipo, Fecha_Registro, Visita_Programada, Elaboracion, Observacion_Elaboracion, Entregado,
+                Manifestacion, Oficio_Subdivision, Escrituras, Licencia_Construccion, Otra, Otra_Nombre, Id_Cliente,
+                Clave_Inmueble, Clave_Empleado);
+                if (res)
+                    Mensaje = "Los datos del Dictamen/Estimación fueron actualizados satisfactoriamente";
+                return res;
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el proceso de actualización de datos del Dictamen/Estimación, es posible"
+                   + " que no se hayan modificado los datos correctamente";
+                return false;
+            }
+        }
+        public bool Eliminar(int Numero)
+        {
+            try
+            {
+                bool res = false;
+                Mensaje = "Ocurrio un error en el proceso de eliminación del Dictamen/Estimación, es posible que no se haya borrado"
+                    + " correctamente";
+                res = dtsEliminar(Numero);
+                if (res)
+                    Mensaje = "El Dictamen/Estimación fue eliminado satisfactoriamente";
+                return res;
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el proceso de eliminación del Dictamen/Estimación, es posible que no se haya borrado"
+                   + " correctamente";
+                return false;
+            }
+        }
+
+        public bool Activar(int Numero)
+        {
+            try
+            {
+                bool res = false;
+                Mensaje = "Ocurrio un error en el proceso de activación del Dictamen/Estimación, es posible que no se haya borrado"
+                    + " correctamente";
+                res = dtsActivar(Numero);
+                if (res)
+                    Mensaje = "El Dictamen/Estimación fue activado satisfactoriamente";
+                return res;
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el proceso de activación del Dictamen/Estimación, es posible que no se haya borrado"
+                    + " correctamente";
+                return false;
+            }
+        }
+
+        public DataTable SelTodos()
+        {
+            try
+            {
+                return dtsSelTodos();
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el proceso de Consultar a todos los Dictamenes/Estimaciones";
+                return null;
+            }
+        }
+
+        public Dictamen_Estimacion[] TableToArray(DataTable Dt)
+        {
+            try
+            {
+                int i = 0;
+                Dictamen_Estimacion[] dicests = new Dictamen_Estimacion[Dt.Rows.Count];
+                foreach (DataRow renglon in Dt.Rows)
+                {
+                    Dictamen_Estimacion dicest = new Dictamen_Estimacion();
+                    if (Dt.Columns.Contains("Numero"))
+                        dicest.Numero = Convert.ToInt16(renglon["Numero"]);
+                    if (Dt.Columns.Contains("Tipo"))
+                        dicest.Tipo = Convert.ToBoolean(renglon["Tipo"]);
+                    if (Dt.Columns.Contains("Fecha_Registro"))
+                        dicest.Fecha_Registro = Convert.ToDateTime(renglon["Fecha_Registro"]);
+                    if (Dt.Columns.Contains("Visita_Programada"))
+                        dicest.Visita_Programada = Convert.ToDateTime(renglon["Visita_Programada"]);
+                    if (Dt.Columns.Contains("Elaboracion"))
+                        dicest.Elaboracion = Convert.ToBoolean(renglon["Elaboracion"]);
+                    if (Dt.Columns.Contains("Observacion_Elaboracion"))
+                        dicest.Observacion_Elaboracion = renglon["Observacion_Elaboracion"].ToString();
+                    if (Dt.Columns.Contains("Entregado"))
+                        dicest.Entregado = Convert.ToBoolean(renglon["Entregado"]);
+                    if (Dt.Columns.Contains("Manifestacion"))
+                        dicest.Manifestacion = Convert.ToBoolean(renglon["Manifestacion"]);
+                    if (Dt.Columns.Contains("Oficio_Subdivision"))
+                        dicest.Oficio_Subdivision = Convert.ToBoolean(renglon["Oficio_Subdivision"]);
+                    if (Dt.Columns.Contains("Escrituras"))
+                        dicest.Escrituras = Convert.ToBoolean(renglon["Escrituras"]);
+                    if (Dt.Columns.Contains("Licencia_Construccion"))
+                        dicest.Licencia_Construccion = Convert.ToBoolean(renglon["Licencia_Construccion"]);
+                    if (Dt.Columns.Contains("Otra"))
+                        dicest.Otra = Convert.ToBoolean(renglon["Otra"]);
+                    if (Dt.Columns.Contains("Otra_Nombre"))
+                        dicest.Otra_Nombre = renglon["Otra_Nombre"].ToString(); ;
+                    if (Dt.Columns.Contains("Id_Cliente"))
+                        dicest.Id_Cliente = Convert.ToInt16(renglon["Id_Cliente"]);
+                    if (Dt.Columns.Contains("Clave_Inmueble"))
+                        dicest.Clave_Inmueble = Convert.ToInt16(renglon["Clave_Inmueble"]);
+                    if (Dt.Columns.Contains("Clave_Empleado"))
+                        dicest.Clave_Empleado = Convert.ToInt16(renglon["Clave_Empleado"]);
+                    if (Dt.Columns.Contains("Eliminado"))
+                        dicest.Eliminado = Convert.ToBoolean(renglon["Eliminado"]);
+                    dicest.Existe = true;
+                    dicests[i] = dicest;
+                    i++;
+                }
+                return dicests;
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en la construcción del arreglo de Dictamen/Estimación";
+                return new Dictamen_Estimacion[0];
+            }
+        }
+
+        #endregion Metodos
+
+    }
+}
