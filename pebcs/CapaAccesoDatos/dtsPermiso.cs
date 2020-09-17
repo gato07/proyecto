6f834b6999034b3ac4a0ecc560df8b4c58eba3cb
Arturo Aguilar <kdt_gato07@hotmail.com>
2020-09-16 20:36:17 -0600

    tabla permisos datos y logica

---
+++ pebcs/CapaAccesoDatos/dtsPermiso.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace CapaAccesoDatos
+{
+    public class dtsPermiso
+    {
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Propiedades
+
+        public int Perfil { get; set; }
+        public string Proceso { get; set; }
+        public string Subproceso { get; set; }
+        public bool Existe { get; set; }
+
+        #endregion Propiedades
+
+        #region Metodos
+
+        public dtsPermiso()
+        {
+            try
+            {
+                Perfil = 0;
+                Proceso = "";
+                Subproceso = "";
+                Existe = false;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public dtsPermiso(int Perfil, string Proceso, string Subproceso)
+        {
+            try
+            {
+                this.Perfil = Perfil;
+                this.Proceso = Proceso;
+                this.Subproceso = Subproceso;
+                Existe = false;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        public DataTable dtsSelXPerfil(int Perfil)
+        {
+            try
+            {
+                DataTable dt = null;
+                Conexion conexion = new Conexion();
+                conexion.Conectar();
+                dt = conexion.Consulta_Seleccion("CALL SP_Permiso_SelXPerfil(" + Perfil + ");").Tables[0];
+                conexion.Desconectar();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        #endregion Metodos
+
+    }
+}
---
+++ pebcs/CapaLogica/Permiso.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using CapaAccesoDatos;
+using System.Text;
+
+namespace CapaLogica
+{
+    public class Permiso : dtsPermiso
+    {
+
+        #region Atributos
+
+        #endregion Atributos
+
+        #region Propiedades
+
+        public string Mensaje { get; set; }
+
+        #endregion Propiedades
+
+        #region Metodos
+
+        public Permiso() : base()
+        {
+            try
+            {
+                Mensaje = "";
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el constructor del Permiso";
+            }
+        }
+
+        public Permiso(int Perfil, string Proceso, string Subproceso) : base(Perfil, Proceso, Subproceso)
+        {
+            try
+            {
+                Mensaje = "";
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el constructor del Permiso";
+            }
+        }
+
+        public DataTable SelXPerfil(int Perfil)
+        {
+            try
+            {
+                return dtsSelXPerfil(Perfil);
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en el proceso de Consultar a todos los Permisos X Perfil";
+                return null;
+            }
+        }
+
+        public Permiso[] TableToArray(DataTable Dt)
+        {
+            try
+            {
+                int i = 0;
+                Permiso[] permisos = new Permiso[Dt.Rows.Count];
+                foreach (DataRow renglon in Dt.Rows)
+                {
+                    Permiso permiso = new Permiso();
+                    if (Dt.Columns.Contains("Perfil"))
+                        permiso.Perfil = Convert.ToInt16(renglon["Perfil"]);
+                    if (Dt.Columns.Contains("Proceso"))
+                        permiso.Proceso = renglon["Proceso"].ToString();
+                    if (Dt.Columns.Contains("Subproceso"))
+                        permiso.Subproceso = renglon["Subproceso"].ToString();
+                    permiso.Existe = true;
+                    permisos[i] = permiso;
+                    i++;
+                }
+                return permisos;
+            }
+            catch (Exception ex)
+            {
+                Mensaje = "Ocurrio un error en la construcción del arreglo de Permisos";
+                return new Permiso[0];
+            }
+        }
+
+        #endregion Metodos
+
+    }
+}
